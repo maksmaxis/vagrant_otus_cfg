11ca66114b28af64ca9686a3f4e09c81cb86754e
Rina <60498923+maksmaxis@users.noreply.github.com>
2022-08-15 16:28:28 +0600

    Create mysql_backup_script.sh
    mysq backup script.

---
+++ mysql/mysql_backup_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+# Mysql backup script
+
+PATH=$PATH:/usr/local/bin
+user='-uroot'
+
+MYSQL='mysql --skip-column-names'
+
+for db in `$MYSQL -e "SHOW DATABASES LIKE '%\_tbl'"`;
+	do
+for tbl in $($MYSQL $user --password $db -e "SHOW TABLES LIKE '%_tbl'");
+	do
+	/usr/bin/mysqldump $user --password --master-data=2 $db  $tbl | gzip -1 > /home/source/mysqldump/dump_$(date +%F_%T_%N).gz
+done    
+done
