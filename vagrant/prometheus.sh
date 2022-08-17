ddf806f7125ec8dedea2b75313579a5f48eef3a9
Rina <60498923+maksmaxis@users.noreply.github.com>
2022-08-17 12:52:17 +0600

    Create prometheus.sh

---
+++ vagrant/prometheus.sh
@@ -0,0 +1,63 @@
+# создаем директорию для rpm файлов
+mkdir /home/vagrant/prometheus/
+
+# скачиваем prometheus
+cd /home/vagrant/prometheus/; curl -LO https://github.com/prometheus/prometheus/releases/download/v2.37.0/prometheus-2.37.0.linux-amd64.tar.gz
+# скачиваем node_exporter
+cd /home/vagrant/prometheus/; curl -LO https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+# скачиваем grafana
+cd /home/vagrant/prometheus/; curl -LO https://dl.grafana.com/enterprise/release/grafana-enterprise-9.1.0-1.x86_64.rpm
+
+# делаем распаковку архивов
+tar -xf /home/vagrant/prometheus/*.tar.gz
+
+# удаляем архивы
+rm -f /home/vagrant/prometheus/*.tar.gz
+
+# создаем пользователся для prometheus
+useradd --no-create-home --shell /usr/sbin/nologin prometheus
+
+# создаем пользователя для node_exporter
+useradd --no-create-home --shell /bin/false node_exporter
+
+# создаем директории для prometheus
+mkdir {/etc/,/var/lib/}prometheus
+
+# копируем конфигурацию сервисов в директории
+
+cd /home/vagrant/prometheus/prometheus-2.37.0.linux-amd64/; cp -r consoles console_libraries prometheus.yml /etc/prometheus/
+cd /home/vagrant/prometheus/prometheus-2.37.0.linux-amd64/; cp prom{etheus, tool} /usr/local/bin/
+cd /home/vagrant/prometheus/node_exporter-1.3.1.linux-amd64/; cp node_exporter /usr/local/bin/
+
+# выдаем права для пользователям
+chown -R prometheus: /etc/prometheus/ /var/lib/prometheus /usr/local/bin/prom*
+chown node_exporter: /usr/local/bin/node_exporter
+
+cp /home/vagrant/vagrant_otus_cfg/prometheus/prometheus.service /etc/systemd/system/
+cp /home/vagrant/vagrant_otus_cfg/prometheus/node_exporter.service /etc/systemd/system/
+
+# перезапустить демона
+systemctl daemon-reload
+
+systemctl start node_exporter
+systemctl start prometheus
+systemctl enable node_exporter
+systemctl enable prometheus
+
+# запрашивать у node_exporter данные каждые 5s
+echo "
+  - job_name: 'node_exporter'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:9100']
+
+" >> /etc/prometheus/prometheus.yml
+
+# перезапустить конфигурацию prometheus
+systemctl reload prometheus
+
+# установка grafana
+sudo yum install -y /home/vagrant/prometheus/grafana-enterprise-9.1.0-1.x86_64.rpm
+# запуск grafana
+systemctl start grafana-server.service
+systemctl enable grafana-server.service
