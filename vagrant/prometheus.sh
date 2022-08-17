cc1438757504252f30eeda6f1d9fdff6b6324f68
Rina <60498923+maksmaxis@users.noreply.github.com>
2022-08-17 22:57:52 +0600

    Update prometheus.sh

--- vagrant/prometheus.sh
+++ vagrant/prometheus.sh
@@ -33,8 +33,8 @@ cd /home/vagrant/prometheus/node_exporter-1.3.1.linux-amd64/; cp node_exporter /
 chown -R prometheus: /etc/prometheus/ /var/lib/prometheus /usr/local/bin/prom*
 chown node_exporter: /usr/local/bin/node_exporter
 
-cp /home/vagrant/vagrant_otus_cfg/prometheus/prometheus.service /etc/systemd/system/
-cp /home/vagrant/vagrant_otus_cfg/prometheus/node_exporter.service /etc/systemd/system/
+cp /home/vagrant/repo/vagrant_otus_cfg/prometheus/prometheus.service /etc/systemd/system/
+cp /home/vagrant/repo/vagrant_otus_cfg/prometheus/node_exporter.service /etc/systemd/system/
 
 # перезапустить демона
 systemctl daemon-reload
