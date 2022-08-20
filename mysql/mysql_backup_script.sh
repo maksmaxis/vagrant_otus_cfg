5f9aba834aadc083a0dfb17f4676f754e1623acb
Rina <60498923+maksmaxis@users.noreply.github.com>
2022-08-20 15:10:06 +0600

    Update mysql_backup_script.sh

--- mysql/mysql_backup_script.sh
+++ mysql/mysql_backup_script.sh
@@ -11,6 +11,6 @@ for db in `$MYSQL -e "SHOW DATABASES LIKE '%\_tbl'"`;
 	do
 for tbl in $($MYSQL $user --password $db -e "SHOW TABLES LIKE '%_tbl'");
 	do
-	/usr/bin/mysqldump $user --password --master-data=2 $db  $tbl | gzip -1 > /home/source/mysqldump/dump_$(date +%F_%T_%N).gz
+	/usr/bin/mysqldump $user --password --master-data=2 $db  $tbl | gzip -1 > /home/vagrant/mysqldump/dump_$(date +%F_%T_%N).gz
 done    
 done
