941770cbf911cf847d990d292de0a05d15862199
Rina <60498923+maksmaxis@users.noreply.github.com>
2022-08-20 16:45:08 +0600

    Update mysql_backup_script.sh

--- mysql/mysql_backup_script.sh
+++ mysql/mysql_backup_script.sh
@@ -7,7 +7,7 @@ user='-uroot'
 
 MYSQL='mysql --skip-column-names'
 
-for db in `$MYSQL -e "SHOW DATABASES LIKE '%\_tbl'"`;
+for db in `$MYSQL -e "SHOW DATABASES LIKE '%\_mysql'"`;
 	do
 for tbl in $($MYSQL $user --password $db -e "SHOW TABLES LIKE '%_tbl'");
 	do
